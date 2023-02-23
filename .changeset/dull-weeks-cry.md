3f2fab2f97fc69c27d4dd7153de63e0741c43762
tpluscode <tpluscode@users.noreply.github.com>
2023-02-23 19:54:29 +0100

    chore: remove deprecated export

---
+++ .changeset/dull-weeks-cry.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/shape-to-query": minor
+---
+
+Removed deprecated export `construct`
--- packages/shape-to-query/index.ts
+++ packages/shape-to-query/index.ts
@@ -1,9 +1,5 @@
 export { shapeToPatterns } from './lib/shapeToPatterns'
 export {
-  /**
-   * @deprecated
-   */
-  constructQuery as construct,
   constructQuery,
   deleteQuery,
 } from './lib/shapeToQuery'
