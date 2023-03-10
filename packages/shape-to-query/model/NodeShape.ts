7748e9a1e7835a0c4db6f5ed9c05a680b35145cd
tpluscode <tpluscode@users.noreply.github.com>
2023-03-10 11:05:26 +0100

    fix: targetNode not bound with sh:and

--- packages/shape-to-query/model/NodeShape.ts
+++ packages/shape-to-query/model/NodeShape.ts
@@ -1,9 +1,8 @@
 import { Variable } from 'rdf-js'
-import { isResource } from 'is-graph-pointer'
 import { sparql, SparqlTemplateResult } from '@tpluscode/sparql-builder'
 import { emptyPatterns, flatten, ShapePatterns, union } from '../lib/shapePatterns'
 import { PropertyShape } from './PropertyShape'
-import { Target, TargetNode } from './target'
+import { Target } from './target'
 import { ConstraintComponent } from './constraint/ConstraintComponent'
 import Shape, { BuildParameters } from './Shape'
 
@@ -22,22 +21,14 @@ export default class extends Shape implements NodeShape {
   }
 
   buildPatterns(arg: BuildParameters): ShapePatterns {
-    let { focusNode, variable } = arg
     let targets: ShapePatterns = emptyPatterns
     if (arg.focusNode.termType === 'Variable') {
-      targets = union(...this.targets.flatMap(target => target.buildPatterns(<any>{ focusNode, variable })))
-    }
-    if (this.targets.length === 1 && this.targets[0] instanceof TargetNode) {
-      const [target] = this.targets
-      if (isResource(target.nodes)) {
-        targets = emptyPatterns
-        focusNode = target.nodes.term
-      }
+      targets = union(...this.targets.flatMap(target => target.buildPatterns(<any>arg)))
     }
 
     let properties: ShapePatterns = emptyPatterns
     if (this.properties.length) {
-      properties = union(...this.properties.map(p => p.buildPatterns({ focusNode, variable })))
+      properties = union(...this.properties.map(p => p.buildPatterns(arg)))
     }
 
     const logical = this.buildLogicalConstraints(arg)
