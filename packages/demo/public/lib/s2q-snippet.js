aa472a52a86fe0adb48d9ceda42496b44bfe7295
tpluscode <tpluscode@users.noreply.github.com>
2023-04-08 22:06:05 +0200

    docs: undelete and improve snippet element

---
+++ packages/demo/public/lib/s2q-snippet.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies,import/no-unresolved */
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/components/tab/tab.js'
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/components/tab-group/tab-group.js'
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/components/tab-panel/tab-panel.js'
+import { setBasePath } from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/utilities/base-path.js'
+import { html, LitElement } from 'https://unpkg.com/lit?module'
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/')
+
+customElements.define('s2q-snippet', class extends LitElement {
+  firstUpdated() {
+    requestAnimationFrame(() => {
+      this.renderRoot.querySelector('sl-tab-group').show('shape')
+    })
+  }
+
+  render() {
+    return html`
+      <sl-tab-group>
+        <sl-tab slot="nav" panel="shape" active>
+          <sl-icon name="box-arrow-in-right"></sl-icon>
+          &nbsp;
+          Shape
+        </sl-tab>
+        <sl-tab slot="nav" panel="query">
+          Query
+          &nbsp;
+          <sl-icon name="box-arrow-right"></sl-icon>
+        </sl-tab>
+        
+        <sl-tab-panel name="shape">
+          <slot name="shape"></slot>
+        </sl-tab-panel>
+        
+        <sl-tab-panel name="query">
+          <slot name="query"></slot>
+        </sl-tab-panel>
+      </sl-tab-group>
+    `
+  }
+})
