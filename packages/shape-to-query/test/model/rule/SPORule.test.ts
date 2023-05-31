ecc350ef068e0fe6d3e984e28e3efe68a51f9d62
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-05-31 13:50:32 +0200

    test: spo rule

---
+++ packages/shape-to-query/test/model/rule/SPORule.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import $rdf from 'rdf-ext'
+import { sparql } from '@tpluscode/sparql-builder'
+import { SPORule } from '../../../model/rule/SPORule.js'
+import { variable } from '../../variable.js'
+import { fakeExpression } from '../nodeExpression/helper.js'
+
+describe('model/rule/SPORule', () => {
+  before(() => import('../../sparql.js'))
+
+  describe('buildPatterns', () => {
+    it('applies multiple filters', () => {
+      // given
+      const filter1 = fakeExpression(args => sparql`${args.subject} filter1 .`)
+      const filter2 = fakeExpression(args => sparql`${args.subject} filter2 .`)
+      const rule = new SPORule({
+        objectFilters: [filter1, filter2],
+        predicateFilters: [filter1, filter2],
+      })
+
+      // when
+      const result = rule.buildPatterns({
+        variable,
+        rootPatterns: undefined,
+        focusNode: $rdf.namedNode('this'),
+      })
+
+      // then
+      expect(sparql`${result.constructClause}\n${result.whereClause}`).to.equalPatterns(`
+        <this> ?p ?o .
+        
+        <this> ?p ?o .
+        ?p filter1 .
+        ?p filter2 .
+        ?o filter1 .
+        ?o filter2 .
+      `)
+    })
+  })
+})
