460bcc9614addcac7538206703994c31b801297d
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-02-13 16:52:19 +0100

    build(deps): updates

---
+++ .changeset/wise-dots-retire.md
@@ -0,0 +1,8 @@
+---
+"@hydrofoil/shape-to-query": patch
+---
+
+Updated dependencies:
+
+1. `@zazuko/env` to `2.0.3`
+
--- packages/shape-to-query/index.ts
+++ packages/shape-to-query/index.ts
@@ -1,10 +1,11 @@
 export { sparql } from '@tpluscode/sparql-builder'
-export { shapeToPatterns, Options } from './lib/shapeToPatterns.js'
+export { shapeToPatterns } from './lib/shapeToPatterns.js'
+export type { Options } from './lib/shapeToPatterns.js'
 export {
   constructQuery,
   deleteQuery,
 } from './lib/shapeToQuery.js'
 export { default as s2q } from './ns.js'
-export { ShapePatterns } from './lib/shapePatterns.js'
+export type { ShapePatterns } from './lib/shapePatterns.js'
 export { default as PropertyShape } from './model/PropertyShape.js'
 export { default as NodeShape } from './model/NodeShape.js'
