69ef993d80d7dd04da1570df32287e76bfe8fcf9
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-12-20 15:04:41 +0100

    feat: remove excessive OPTIONAL patterns

---
+++ .changeset/rare-deers-tell.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/sparql-processor": patch
+---
+
+`DuplicatePatternRemover` not removes exactly equal `OPTIONAL` patterns (but only comparing BGPs)
---
+++ packages/processor/test/DuplicatePatternRemover.test.ts
@@ -0,0 +1,42 @@
+import rdf from '@zazuko/env/web.js'
+import { expect } from 'chai'
+import { DuplicatePatternRemover } from '../DuplicatePatternRemover.js'
+import { loadQuery, stringifyQuery } from './lib/query.js'
+
+describe('DuplicatePatternRemover', () => {
+  it('removes equal consecutive OPTIONAL graph patterns', () => {
+    // given
+    const processor = new DuplicatePatternRemover(rdf)
+
+    // when
+    const processed = processor.process(loadQuery('duplicate-optional.rq'))
+
+    // then
+    expect(stringifyQuery(processed))
+      .to.deep.equal(stringifyQuery(loadQuery('duplicate-optional.expected.rq')))
+  })
+
+  it('removes equal non-consecutive OPTIONAL graph patterns', () => {
+    // given
+    const processor = new DuplicatePatternRemover(rdf)
+
+    // when
+    const processed = processor.process(loadQuery('duplicate-optional.not-adjacent.rq'))
+
+    // then
+    expect(stringifyQuery(processed))
+      .to.deep.equal(stringifyQuery(loadQuery('duplicate-optional.not-adjacent.expected.rq')))
+  })
+
+  it('preserves OPTIONAL graph patterns if they are not exactly the same', () => {
+    // given
+    const processor = new DuplicatePatternRemover(rdf)
+
+    // when
+    const processed = processor.process(loadQuery('similar-optional.rq'))
+
+    // then
+    expect(stringifyQuery(processed))
+      .to.deep.equal(stringifyQuery(loadQuery('similar-optional.rq')))
+  })
+})
