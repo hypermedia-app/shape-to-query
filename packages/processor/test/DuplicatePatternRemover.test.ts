0e3bd2d37a99a4ef66820cd3f43de476a820cadd
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-12-20 15:11:08 +0100

    test: non-bgp patterns in optional

--- packages/processor/test/DuplicatePatternRemover.test.ts
+++ packages/processor/test/DuplicatePatternRemover.test.ts
@@ -39,4 +39,16 @@ describe('DuplicatePatternRemover', () => {
     expect(stringifyQuery(processed))
       .to.deep.equal(stringifyQuery(loadQuery('similar-optional.rq')))
   })
+
+  it('preserves OPTIONAL graph patterns if they contain non-BGP patterns', () => {
+    // given
+    const processor = new DuplicatePatternRemover(rdf)
+
+    // when
+    const processed = processor.process(loadQuery('non-bgp-optional.rq'))
+
+    // then
+    expect(stringifyQuery(processed))
+      .to.deep.equal(stringifyQuery(loadQuery('non-bgp-optional.rq')))
+  })
 })
