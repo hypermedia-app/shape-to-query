f4d3e9576b9e6035952def8e6ff59a30693613a0
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-12-30 12:29:34 +0100

    test: fuseki query, real data

--- packages/shape-to-query/test/smoke.test.ts
+++ packages/shape-to-query/test/smoke.test.ts
@@ -1,17 +1,21 @@
 import * as path from 'node:path'
 import * as url from 'node:url'
+import module from 'module'
 import StreamClient from 'sparql-http-client'
 import * as compose from 'docker-compose/dist/v2.js'
 import waitOn from 'wait-on'
-import $rdf from '@zazuko/env'
+import $rdf from '@zazuko/env-node'
 import { sh, schema } from '@tpluscode/rdf-ns-builders'
 import { expect } from 'chai'
 import { constructQuery } from '../lib/shapeToQuery.js'
 import { parse } from './nodeFactory.js'
 
+const require = module.createRequire(import.meta.url)
+
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const client = new StreamClient({
   endpointUrl: 'http://localhost:3030/s2q/query',
+  storeUrl: 'http://localhost:3030/s2q/data',
 })
 
 before(async function () {
@@ -22,6 +26,7 @@ before(async function () {
   await waitOn({
     resources: ['http://localhost:3030'],
   })
+  await client.store.post($rdf.fromFile(require.resolve('tbbt-ld/dist/tbbt.nt')))
 })
 
 describe('@hydrofoil/shape-to-query', () => {
@@ -41,6 +46,6 @@ describe('@hydrofoil/shape-to-query', () => {
     const result = await $rdf.dataset().import(client.query.construct(constructQuery(shape)))
 
     // then
-    expect(result.size).to.equal(0)
+    expect(result.size).to.equal(19)
   })
 })
