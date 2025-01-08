f4fc6086cd8f0834af61f1efd5db326aaa570f62
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-01-08 18:18:15 +0100

    feat: optimise triple rules

--- packages/shape-to-query/model/rule/TripleRule.ts
+++ packages/shape-to-query/model/rule/TripleRule.ts
@@ -1,13 +1,15 @@
+/* eslint-disable camelcase */
 import $rdf from '@zazuko/env/web.js'
 import type { GraphPointer } from 'clownface'
 import { sh } from '@tpluscode/rdf-ns-builders/loose'
 import { isGraphPointer } from 'is-graph-pointer'
 import type { Pattern } from 'sparqljs'
+import type { Term, Variable } from '@rdfjs/types'
 import type { NodeExpression } from '../nodeExpression/NodeExpression.js'
 import { PatternBuilder } from '../nodeExpression/NodeExpression.js'
 import type { ShapePatterns } from '../../lib/shapePatterns.js'
 import type { ModelFactory } from '../ModelFactory.js'
-import type { Rule, Parameters } from './Rule.js'
+import type { Rule, Parameters as BuildParameters } from './Rule.js'
 
 export default class TripleRule implements Rule {
   constructor(public subject: NodeExpression, public predicate: NodeExpression, public object: NodeExpression) {
@@ -31,32 +33,59 @@ export default class TripleRule implements Rule {
     )
   }
 
-  buildPatterns({ focusNode, variable, rootPatterns }: Parameters): ShapePatterns {
+  buildPatterns({ focusNode, variable, rootPatterns }: BuildParameters): ShapePatterns {
+    const args = { subject: focusNode, variable, rootPatterns }
     const builder = new PatternBuilder()
 
-    const subject = builder.build(this.subject, { subject: focusNode, variable, rootPatterns })
-    const predicate = builder.build(this.predicate, { subject: focusNode, variable, rootPatterns })
-    const object = builder.build(this.object, { subject: focusNode, variable, rootPatterns })
+    let constructSubject = getInlineTerm(this.subject, [args, builder], 'NamedNode', 'BlankNode')
+    let constructPredicate = getInlineTerm(this.predicate, [args, builder], 'NamedNode')
+    let constructObject = getInlineTerm(this.object, [args, builder], 'NamedNode', 'BlankNode', 'Literal')
 
-    const whereClause: Pattern[] = [
-      ...rootPatterns,
-      ...subject.patterns,
-      ...predicate.patterns,
-      ...object.patterns,
-    ].map(pattern => {
-      if (pattern.type === 'query') {
-        return {
-          type: 'group',
-          patterns: [pattern],
-        }
-      }
+    const whereClause: Pattern[] = []
+    if (!constructSubject || !constructPredicate || !constructObject) {
+      whereClause.push(...rootPatterns)
+    }
 
-      return pattern
-    })
+    function buildExpression(expression: NodeExpression): Variable {
+      const { patterns, object } = builder.build(expression, args)
+      whereClause.push(...patterns)
+      return object
+    }
+
+    constructSubject = constructSubject || buildExpression(this.subject)
+    constructPredicate = constructPredicate || buildExpression(this.predicate)
+    constructObject = constructObject || buildExpression(this.object)
 
     return {
-      constructClause: [$rdf.quad(subject.object, predicate.object, object.object)],
-      whereClause,
+      constructClause: [$rdf.quad(constructSubject, constructPredicate, constructObject)],
+      whereClause: whereClause.map(pattern => {
+        if (pattern.type === 'query') {
+          return {
+            type: 'group',
+            patterns: [pattern],
+          }
+        }
+
+        return pattern
+      }),
     }
   }
+}
+
+type ArrayToUnion<T> = T extends (infer U)[] ? U : never
+type LimitedTerm<T extends Term['termType']> = Term & { termType: ArrayToUnion<T> }
+
+function getInlineTerm<T extends Term['termType']>(expression: NodeExpression, args: Parameters<NodeExpression['buildInlineExpression']>, ...termType: T[]): Variable | LimitedTerm<T> | undefined {
+  const result = expression.buildInlineExpression?.(...args)
+  if (result && !result.patterns && 'termType' in result.inline) {
+    if (result.inline.termType === 'Variable') {
+      return result.inline
+    }
+
+    if ((termType as string[]).includes(result.inline.termType)) {
+      return result.inline as LimitedTerm<T>
+    }
+  }
+
+  return undefined
 }
