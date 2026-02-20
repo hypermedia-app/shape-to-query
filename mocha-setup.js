aa10d830e5e9bd04902f123ea6c1abdd328706fe
tpluscode <tpluscode@users.noreply.github.com>
2026-02-20 16:28:43 +0100

    build(deps-dev): remove chai-subset which is now included in chai

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,8 +1,6 @@
 import * as chai from 'chai';
 import chaiString from 'chai-string';
 import sinonChai from 'sinon-chai';
-import subset from 'chai-subset';
 
 chai.use(chaiString);
 chai.use(sinonChai);
-chai.use(subset);
